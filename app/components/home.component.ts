8ca59e25e8aba14b6edf3609f4d9f9f78ace5580
Sai Puli <sai.puli@samtec.com>
2017-04-11 14:36:52 -0400

    make necessary changes to call protected api

--- app/components/home.component.ts
+++ app/components/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {AdalService} from 'ng2-adal/core';
 import { Http, Response }          from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -9,25 +9,30 @@ import 'rxjs/add/operator/map';
   selector: 'home',
   template: `<div protected>
   <h1>
-    This is the dashboard page. - {{this.adalService.userInfo.userName}}
+    Welcome - {{this.adalService.userInfo.userName}}
   </h1>
-  <table class="table table-striped table-bordered table-condensed table-hover">
+  <h3 [hidden]="!valuesError"> error with values- {{valuesError}} </h3>
+  <table class="table table-striped table-bordered table-condensed table-hover" [hidden]="error">
     <thead>
         <tr>
-            <th>Claim</th>
-            <th>Value</th>
+            <th>Values</th>
         </tr>
     </thead>
     <tbody>
-        <tr data-ng-repeat="claim in this.adalService.claims">
-            <td>{{claim.key}}</td>
-            <td>{{claim.value}}</td>
-        </tr>
+      <tr *ngFor="let hero of values" (click)="getDetails()"><td>{{hero}}</td></tr>
     </tbody>
   </table>
+  <h3 [hidden]="!detailsError"> error with details - {{detailsError}} </h3>
+  <div>
+    <span>{{details}}</span>
+  </div>
   <button (click)="logOut()">Logout</button></div>`
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  values: string[];
+  valuesError = '';
+  details: string;
+  detailsError = '';
 
   constructor(
     private adalService: AdalService,
@@ -36,15 +41,61 @@ export class HomeComponent {
     console.log('Entering home');
   }
 
+  ngOnInit() {
+    this.getValues().subscribe((data: string[]) => this.values = data, (err: any) => this.valuesError = err);
+  }
+
   public logOut() {
+    this.adalService.clearCache();
     this.adalService.logOut();
   }
 
-  getHeroes(): Observable<string[]> {
-    return this.http.get('http://localhost:5000/api/values')
+  getValues(): Observable<string[]> {
+    let h = this.getHeaders('http://localhost:5000/api/values');
+    return this.http.get('http://localhost:5000/api/values', {headers: h})
                     .map(function(res: Response){
-                      let body = res;
+                      let body = res.json();
                       return body || [];
+                    })
+                    .catch(function(err: any){
+                      return Observable.throw(err.statusText);
                     });
+  }
+
+  getDetails() {
+    console.log('getting details');
+    let h = this.getHeaders('http://localhost:5000/api/values/5');
+    (this.http.get('http://localhost:5000/api/values/5', {headers: h})
+                    .map(function(res: Response){
+                      let body = res.text();
+                      console.log(body);
+                      return body || '';
+                    })
+                    .catch(function(err: any){
+                      return Observable.throw(err.statusText);
+                    })).subscribe((data: string) => this.details = data, (err: any) => this.detailsError = err);
+  }
+
+  private getHeaders(url: string): any {
+    let headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      };
+
+    let resource = this.adalService.GetResourceForEndpoint(url);
+    // console.log(resource);
+    let cachedToken = this.adalService.getCachedToken(resource);
+    // console.log('Cached: ' + cachedToken);
+    if (cachedToken) {
+      headers['Authorization'] = 'Bearer ' + cachedToken;
+    } else {
+      this.adalService
+      .acquireToken(resource)
+      .subscribe((token: string) => {
+        console.log('Acquired: ' + token);
+        headers['Authorization'] = 'Bearer ' + token;
+      });
+    }
+    return headers;
   }
 }
--- app/services/secret.service.ts
+++ app/services/secret.service.ts
@@ -3,11 +3,17 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class SecretService {
   public get adalConfig(): any {
+    let endpoints = {
+      // Map the location of a request to an API to a the identifier of the associated resource
+      'http://localhost:5000/': 'https://pulis.co/fd7170b0-2781-4e42-a928-ec114334108d',
+    };
     return {
       tenant: '56d9ab50-1409-48a1-99c9-e91dfdc9aec9',
       clientId: '45690a9f-e653-49f8-ac1a-387c33ebf8a9',
       redirectUri: window.location.origin + '/',
-      postLogoutRedirectUri: window.location.origin + '/'
+      postLogoutRedirectUri: window.location.origin + '/',
+      extraQueryParameter: 'nux=1',
+      endpoints: endpoints
     };
   }
 }
